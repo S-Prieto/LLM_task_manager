f47c4ce4a30bb1e5d28af35a473c941004e2db11
Samuel A. Prieto <samuel.prieto@nyu.edu>
2024-05-27 12:14:35 +0400

    Added multi_robot_support

--- LLM_task_manager/command_parser.py
+++ LLM_task_manager/command_parser.py
@@ -21,28 +21,37 @@ class ScheduleCommandsSubscriber(Node):
 
     def listener_callback(self, msg):
         self.get_logger().info(f'Received message: {msg.data}')
-        step_, current_location_, action_, internal_cargo_, placed_bricks_, battery_ = self.parse_message(msg.data)
-        self.get_logger().info(f'Parsed values - Step: {step_}, Current Location: {current_location_}, Action: {action_}, Internal Cargo: {internal_cargo_}, Placed Bricks: {placed_bricks_}, Battery: {battery_}')
-        self.publish_commanded_action(step_, current_location_, action_, internal_cargo_, placed_bricks_, battery_)
+        parsed_data = self.parse_message(msg.data)
+        if parsed_data:
+            (step_, current_location_, action_, internal_cargo_, 
+             placed_bricks_, battery_) = parsed_data
+            self.get_logger().info(f'Parsed values - Step: {step_}, '
+                                   f'Current Locations: {current_location_}, '
+                                   f'Actions: {action_}, Internal Cargos: {internal_cargo_}, '
+                                   f'Placed Bricks: {placed_bricks_}, Batteries: {battery_}')
+            for i, (current_location, action, cargo, battery) in enumerate(zip(current_location_, action_, internal_cargo_, battery_)):
+                robot_id = f'robot_{i+1}'
+                self.publish_commanded_action(robot_id, step_, current_location, action, cargo, placed_bricks_, battery)
+
 
     def parse_message(self, message):
         pattern = r'\{STEP (\d+), \[(.*?)\], \[(.*?)\], \[(.*?)\], (\d+), \[(.*?)\]\}'
         match = re.match(pattern, message)
         if match:
             step_ = int(match.group(1))
-            current_location_ = match.group(2)
-            action_ = match.group(3)
-            internal_cargo_ = match.group(4)
+            current_location_ = [loc.strip() for loc in match.group(2).split(',')]
+            action_ = [act.strip() for act in match.group(3).split(',')]
+            internal_cargo_ = [cargo.strip() for cargo in match.group(4).split(',')]
             placed_bricks_ = int(match.group(5))
-            battery_ = match.group(6)
+            battery_ = [bat.strip() for bat in match.group(6).split(',')]
             return step_, current_location_, action_, internal_cargo_, placed_bricks_, battery_
         else:
             self.get_logger().error('Failed to parse message')
-            return None, None, None, None, None, None
-        
-    def publish_commanded_action(self, step_, current_location_, action_, internal_cargo_, placed_bricks_, battery_):
+            return None
+                
+    def publish_commanded_action(self, robot_id, step_, current_location_, action_, internal_cargo_, placed_bricks_, battery_):
         msg = TaskAction()
-        msg.robot = 'robot_1'
+        msg.robot = robot_id
         if action_.startswith("MOVE_"):
             msg.type = 'navigate'
             if action_ == "MOVE_C":
@@ -57,6 +66,7 @@ class ScheduleCommandsSubscriber(Node):
         elif action_ == "CHARGE":
             msg.type = 'charge'
             msg.target_location = ""
+
 
         else:
             self.get_logger().error(f'Unsupported action type: {action_}')
